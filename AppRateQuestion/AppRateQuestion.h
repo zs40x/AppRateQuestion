4fc296d2e7bbb4018f83843e733b189a26c2f6f4
Stefan Mehnert <StefanMehnert@gmail.com>
2017-04-10 05:27:04 +0200

    Initial Commit

---
+++ AppRateQuestion/AppRateQuestion.h
@@ -0,0 +1,19 @@
+//
+//  AppRateQuestion.h
+//  AppRateQuestion
+//
+//  Created by Stefan Mehnert on 10.04.17.
+//  Copyright © 2017 Stefan Mehnert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppRateQuestion.
+FOUNDATION_EXPORT double AppRateQuestionVersionNumber;
+
+//! Project version string for AppRateQuestion.
+FOUNDATION_EXPORT const unsigned char AppRateQuestionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppRateQuestion/PublicHeader.h>
+
+
